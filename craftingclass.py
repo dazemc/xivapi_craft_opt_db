d32d5e5634d90a4233f8de68124a3c141122fa3a
dazemc <daazedjmcfarland@gmail.com>
2023-02-05 04:06:44 -0800

    Buffs() and more clean up

--- craftingclass.py
+++ craftingclass.py
@@ -1,13 +1,17 @@
-#I just learned what classes are today and I think this will be best to get the appropriate key/values for ffxiv crafting
-#Will need to iterate through each "Name" key to find the values
-#Bonuses.*.Value is the percentage
-class Meals:
-
-    def __init__(self, cp_percent, cp_value, craft_percent, craft_value, hq, id, name):
+# I just learned what classes are today and I think this will be best to get the appropriate key/values for ffxiv
+# crafting Will need to iterate through each "Name" key to find the values Bonuses.*.Value is the percentage
+class Buffs:
+    def __init__(self, cp_percent = 0, cp_value = 0, craft_percent = 0, craft_value = 0, control_percent = 0, control_value = 0, name = 0):
         self.cp_percent = cp_percent
         self.cp_value = cp_value
         self.crafsmanship_percent = craft_percent
         self.crafsmanship_value = craft_value
-        self.hq = hq
-        self.id = id
-        self.name = {name}
+        self.control_percent = control_percent
+        self.control_value = control_value
+        # self.hq = hq
+        # self.id = id
+        self.name = name
+
+
+
+
--- xivapi_buffs.py
+++ xivapi_buffs.py
@@ -1,91 +1,111 @@
 import json
 import requests
 from collections import defaultdict
-from craftingclass import Meals
+from craftingclass import Buffs
 
-#This is still a work in progress and will be updated as I gain time/knowledge
-#Bare with me, I'm new
+# Almost there!
 
-
-
-#payload endpoing
+# payload endpoint
 url = "https://xivapi.com/search"
 
-#Headers, duh
+# Headers, duh
 headers = defaultdict()
 headers["Accept"] = "application/json"
 headers["Content-Type"] = "application/json"
 
-#json payloads meals/meds
+# json payloads meals/meds
 meals = {
- "indexes": "item",
- "columns": "Name,Bonuses",
-  "body": {
-    "query": {
-        "bool" : {
-            "should" : [
-                {"match" : { "Bonuses.CP.Relative" : "true" }},
-                {"match" : { "Bonuses.Control.Relative" : "true" }},
-                {"match" : { "Bonuses.Craftsmanship.Relative" : "true" }}
+    "indexes": "item",
+    "columns": "Name,Bonuses",
+    "body": {
+        "query": {
+            "bool": {
+                "should": [
+                    {"match": {"Bonuses.CP.Relative": "true"}},
+                    {"match": {"Bonuses.Control.Relative": "true"}},
+                    {"match": {"Bonuses.Craftsmanship.Relative": "true"}}
 
-            ],
-           "must_not": [
-                 {"match" : { "ItemSearchCategory.Name_en" : "Medicine" }} 
+                ],
+                "must_not": [
+                    {"match": {"ItemSearchCategory.Name_en": "Medicine"}}
 
-            ]
-        }
-    },
-    "from": 0,
-    "size": 100
-}
+                ]
+            }
+        },
+        "from": 0,
+        "size": 100
+    }
 }
 
 meds = {
- "indexes": "item",
- "columns": "Name,Bonuses",
-  "body": {
-    "query": {
-        "bool" : {
-            "should" : [
-                {"match" : { "Bonuses.CP.Relative" : "true" }},
-                {"match" : { "Bonuses.Control.Relative" : "true" }},
-                {"match" : { "Bonuses.Craftsmanship.Relative" : "true" }}
+    "indexes": "item",
+    "columns": "Name,Bonuses",
+    "body": {
+        "query": {
+            "bool": {
+                "should": [
+                    {"match": {"Bonuses.CP.Relative": "true"}},
+                    {"match": {"Bonuses.Control.Relative": "true"}},
+                    {"match": {"Bonuses.Craftsmanship.Relative": "true"}}
 
-            ],
-           "must_not": [
-                 {"match" : { "ItemSearchCategory.Name_en" : "Meals" }} 
+                ],
+                "must_not": [
+                    {"match": {"ItemSearchCategory.Name_en": "Meals"}}
 
-            ]
-        }
-    },
-    "from": 0,
-    "size": 100
+                ]
+            }
+        },
+        "from": 0,
+        "size": 100
+    }
 }
-}
 
-
-#Save the responses
+# Save the responses
 meals_request = requests.post(url, headers=headers, json=meals)
 meds_request = requests.post(url, headers=headers, json=meds)
 
-#Clean it up a bit and prepping for class to convert to ffxiv crafters' data
+
+# Clean it up a bit and prepping for class to convert to ffxiv crafters' data
 def clean_up(resp):
     resp = (json.loads(resp.text))
     del resp["Pagination"]
     resp = resp["Results"]
     return resp
 
-meals_request = clean_up(meals_request)
+
+# Rename keys
+def shuffle(buff):
+    for i in range(len(buff)):
+        if i <= 0:
+            temp_list = []
+        else:
+            s_cp_percent = buff[i].get("Bonuses", {}).get("CP", {}).get("Value")
+            s_cp_value = buff[i].get("Bonuses", {}).get("CP", {}).get("Max")
+            s_craft_percent = buff[i].get("Bonuses", {}).get("Craftsmanship", {}).get("Value")
+            s_craft_value = buff[i].get("Bonuses", {}).get("Craftsmanship", {}).get("Max")
+            s_control_percent = buff[i].get("Bonuses", {}).get("Control", {}).get("Value")
+            s_control_value = buff[i].get("Bonuses", {}).get("Control", {}).get("Max")
+            s_name = buff[i].get("Name")
+            temp_list.append(vars(
+                Buffs(s_cp_percent, s_cp_value, s_craft_percent, s_craft_value, s_control_percent, s_control_value,
+                      s_name)))
+    return temp_list
+
+
+# Calls
 meds_request = clean_up(meds_request)
+meals_request = clean_up(meals_request)
+meds = shuffle(meds_request)
+meals = shuffle(meals_request)
 
+# TODO: Remove None values
 
-
-#print out responses into respective files, and make them if they are not there
+# print out responses into respective files, and make them if they are not there
 with open("Meal.json", mode="w+", encoding="utf-8") as my_file:
     my_file.seek(0)
-    my_file.write(json.dumps(meals_request, indent=2, sort_keys=True, ensure_ascii=False))
+    my_file.write(json.dumps(meals, indent=2, sort_keys=True, ensure_ascii=False))
     my_file.truncate()
 with open("Medicine.json", mode="w+", encoding="utf-8") as my_file:
     my_file.seek(0)
-    my_file.write(json.dumps(meds_request, indent=2, sort_keys=True, ensure_ascii=False))
+    my_file.write(json.dumps(meds, indent=2, sort_keys=True, ensure_ascii=False))
     my_file.truncate()
